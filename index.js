a36f025aecb2b0c83b898349f5aaa4c7dcb159d8
okanozzzturk <41028872+okanozzzturk@users.noreply.github.com>
2024-01-15 22:59:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+ReactDOM.render(<h1>Merhaba,ben Okan!</h1>, document.getElementById("root"))
+
+/* const myRoot = document.getElementById("root")
+
+const h1 = document.createElement("h1")
+h1.textContent = "vanilla js training"
+h1.className = "header"
+
+myRoot.appendChild(h1) */
